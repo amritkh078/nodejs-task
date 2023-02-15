ddc56b94edda15e57eab5f7d519732f32cad2eb8
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-15 16:44:11 +0545

    export all components

---
+++ mern/client/src/components/index.js
@@ -0,0 +1,9 @@
+// Purpose: To export all components in one file
+
+export { default as UserName } from "./UserName";
+export { default as Profile } from "./Profile";
+export { default as Password } from "./Password";
+export { default as Reset } from "./Reset";
+export { default as Register } from "./Register";
+export { default as PageNotFound } from "./PageNotFound";
+export { default as Recovery } from "./Recovery";
