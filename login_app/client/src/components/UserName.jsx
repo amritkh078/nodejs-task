02dbd5f27538ccdf47a03fc397b5614e1f281f44
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-19 19:33:07 +0545

    .

--- login_app/client/src/components/UserName.jsx
+++ login_app/client/src/components/UserName.jsx
@@ -8,7 +8,7 @@ const UserName = () => {
         <div>
           <div className="title flex flex-col items-center">
             <h4 className='text-5xl font-bold'>Hello</h4>
-            <span className='py-4 text-xl w-2/3 text-center text-gray-500'> Explore</span>
+            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>Explore</span>
           </div>
 
           <form className='py-1'>
@@ -18,7 +18,7 @@ const UserName = () => {
 
             <div className='textbox'>
               <input type='text' placeholder='username'/>
-              <button type='submit'>Lets go</button>
+              <button type='submit'>Submit</button>
             </div>
 
             <div className="text-center py-4">
