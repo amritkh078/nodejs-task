7a6ad58d93e8f8dce86f713c1c5fc238d6a49dbb
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-21 13:26:46 +0545

    deleted

--- login_app/client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as UserName} from './UserName';
-export {default as Reset} from './Reset';
-export {default as Register} from './Register';
-export {default as Recovery} from './Recovery';
-export {default as Profile} from './Profile';
-export {default as Password} from './Password';
-export {default as PageNotFound} from './PageNotFound';
