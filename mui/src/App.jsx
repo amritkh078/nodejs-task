1941962c1e10e3703034a08bdf730c5ec35df4a8
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-21 22:01:48 +0545

    .

--- mui/src/App.jsx
+++ mui/src/App.jsx
@@ -1,18 +1,8 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import {HomePage, Dashboard,Login,Register} from './pages'
 
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <div>App</div>
   )
 }
 
