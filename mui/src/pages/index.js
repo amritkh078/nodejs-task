3f2030218df4e73b360f9b1b7476b883c7660746
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-23 20:51:20 +0545

    exp pages

---
+++ mui/src/pages/index.js
@@ -0,0 +1,6 @@
+
+export {default as Login} from './Login';
+export {default as Register} from './Register';
+export {default as Home} from './Home';
+export {default as Dashboard} from './Dashboard';
+export {default as ForgotPassword} from './ForgotPassword';
