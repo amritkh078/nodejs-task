226302e7b51d0b0f84534b6e6fb3f0a2afb100b7
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-23 20:51:31 +0545

    .

--- mui/src/App.jsx
+++ mui/src/App.jsx
@@ -1,9 +1,30 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import {Home, Login, Register, Dashboard, ForgotPassword} from "./pages";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
