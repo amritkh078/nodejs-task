7f3485dd4ea9db3bcbc6be879ce1ddfb0314ee70
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-23 20:51:26 +0545

    btn

---
+++ mui/src/components/Logout.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const Logout = () => {
+  return (
+    <Button variant='contained' href='/'>Logout</Button>
+  )
+}
+
+export default Logout
